4c5da04438ec2e1dcdab3af1037ac22db259459d
kanglaeheon <kanglaeheon@gmail.com>
2021-12-28 01:20:30 +0900

    Feat: 추가 Page [id] 추가 Component ItemList, Item

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,19 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import Axios from "axios";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import ItemList from "../src/component/ItemList";
+import { Divider, Header } from "semantic-ui-react";
 
 export default function Home() {
+  const [list, setList] = useState([]);
   const API_URL =
     "http://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline";
 
   function getData() {
     Axios.get(API_URL).then((res) => {
-      console.log(res);
+      console.log(res.data);
+      setList(res.data);
     });
   }
 
@@ -22,6 +26,16 @@ export default function Home() {
       <Head>
         <title>HOME | kanglaeheon</title>
       </Head>
+      <Header as="h3" style={{ paddingTop: 40 }}>
+        베스트 상품
+      </Header>
+      <Divider />
+      <ItemList list={list.slice(0, 9)} />
+      <Header as="h3" style={{ paddingTop: 40 }}>
+        신상품
+      </Header>
+      <Divider />
+      <ItemList list={list.slice(9)} />
     </div>
   );
 }
